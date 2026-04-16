ee61305b580aa4ebb1e3c2f4f41af76904a3df7d
LeonardPertsch <leonard.pertsch@gmx.de>
2026-04-16 16:46:38 +0200

    Time: 0 ms (100%), Space: 12.4 MB (28.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def __init__(self):
+        self.memo = {}
+
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        if n in self.memo:
+            return self.memo[n]
+
+        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
