814e28041f9d5d6012440d1cbb9542d5d2c484c4
LeonardPertsch <leonard.pertsch@gmx.de>
2026-04-16 12:15:29 +0200

    Time: 0 ms (100%), Space: 12.4 MB (34.91%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+        result = [[1]]
+        for i in range(1,numRows):
+            prev = result[-1]
+            nextRow= [1]
+            for j in range(len(prev)-1):
+                nextRow.append(prev[j] + prev[j+1])
+            nextRow.append(1)
+            result.append(nextRow)
+        return result
+        
+
+
